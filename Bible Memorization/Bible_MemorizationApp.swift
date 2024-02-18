726295fb3391f0c02deffecc96555513bdbbd886
Chad Wallace <cewall0@gmail.com>
2024-02-18 14:47:55 -0500

    Initial Commit

---
+++ Bible Memorization/Bible_MemorizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_MemorizationApp.swift
+//  Bible Memorization
+//
+//  Created by Chad Wallace on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Bible_MemorizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bible Memorization/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bible Memorization
+//
+//  Created by Chad Wallace on 2/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
